608b7002b33680478293c86cef0f00bac4171f4a
verle <verle.poa@gmail.com>
2013-08-29 16:35:10 -0300

    troca de pasta do admin.py e retirada de parâmetros a mais no settings.py

--- ProjetoTarde/settings.py
+++ ProjetoTarde/settings.py
@@ -121,8 +121,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'concursos',
-    'Fiscais',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
