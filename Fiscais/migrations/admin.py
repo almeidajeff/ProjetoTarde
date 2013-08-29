211e60aa26d808bf1153bad3f381e311acd21880
verle <verle.poa@gmail.com>
2013-08-29 16:19:04 -0300

    criação do admin para sala e provas

---
+++ Fiscais/migrations/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from models import *
+
+
+#admin.site.register(Concurso)
+#admin.site.register(Inscricao)
+#admin.site.register(Candidato)
+admin.site.register(Prova)
+admin.site.register(Sala)
