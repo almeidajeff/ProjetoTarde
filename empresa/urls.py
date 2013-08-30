c4babbd43704ac52676d1c371ef167d0d8029d01
verle <verle.poa@gmail.com>
2013-08-30 16:15:30 -0300

    reportlab - pdf

--- empresa/urls.py
+++ empresa/urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-import views
+import views 
 
 urlpatterns = patterns('',
                        url(r'^criar/$', views.FuncionarioCreateView.as_view(), name='criar'),
                        url(r'^atualizar/(?P<pk>\d+)\$', views.FuncionarioUpdateView.as_view(), name='atualizar'),
+                       url(r'^pdf/$', views.hello_pdf),
                        )
--- empresa/views.py
+++ empresa/views.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from django.views.generic.edit import FormView
 from django.http import HttpResponse
 from django.views.generic import View
 from django.views.generic import CreateView, UpdateView
 from models import Funcionario
 from forms import FuncionarioForm
-
+from reportlab.pdfgen import canvas
 
 class FuncionarioCreateView(CreateView):
     model = Funcionario
@@ -15,5 +17,14 @@ class FuncionarioUpdateView(UpdateView):
     model = Funcionario
     sucesso_url = 'update.html'
     form_class = FuncionarioForm
+    
+def hello_pdf(request):
+    response = HttpResponse(mimetype='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=hello.pdf'
+    p = canvas.Canvas(response)
+    p.drawString(100,100,'Olá palavra!!')
+    p.showPage()
+    p.save()
+    return response
     
     
